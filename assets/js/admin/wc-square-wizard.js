1dc8b47bdcbab48d8733972a4cb5260bd643cef2
faisal-alvi <faisalalvi33@gmail.com>
2024-04-02 18:29:31 +0530

    add/update the default CSS and JS files

--- assets/js/admin/wc-square-wizard.js
+++ assets/js/admin/wc-square-wizard.js
@@ -6,18 +6,9 @@
  * @since 2.0.0
  */
 jQuery( document ).ready( ( $ ) => {
-
-
-	alert('hey 356');
-
 	const typenow = window.typenow || '';
 	const pagenow = window.pagenow || '';
 	const __ = wp.i18n.__
 
-	// bail if not on product admin pages.
-	if ( 'product' !== typenow ) {
-		return;
-	}
-
-		
+	
 } );
