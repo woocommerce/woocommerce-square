3a206601f63a13201635c13f05c15098a4282788
faisal-alvi <faisalalvi33@gmail.com>
2024-04-15 22:26:41 +0530

    `ConnectSetup` onboarding step

---
+++ assets/new-user-experience/onboarding/steps/connect-square/index.js
@@ -0,0 +1,58 @@
+/**
+ * External dependencies.
+ */
+import { __ } from '@wordpress/i18n';
+import { Button } from '@wordpress/components';
+
+/**
+ * Internal dependencies.
+ */
+import './index.scss';
+import {
+	SectionTitle,
+	SectionDescription,
+} from '../../../components';
+import { usePaymentGatewayData, } from '../../hooks';
+
+export const ConnectSetup = ( { updateStep } ) => {
+	const {
+		settingsLoaded,
+		paymentGatewayData,
+
+		setCreditCardData,
+		setDigitalWalletData,
+		setGiftCardData,
+	} = usePaymentGatewayData();
+
+	const {
+		enabled,
+		enable_digital_wallets,
+		enable_gift_cards,
+	} = paymentGatewayData;
+
+	if ( ! settingsLoaded ) {
+		return null;
+	}
+
+	return (
+		<div className="woo-square-onbarding__payment-settings">
+			<div className="woo-square-onbarding__payment-settings--right">
+				<div className="woo-square-onbarding__payment-settings__toggles">
+					<SectionTitle title={ __( 'Thanks for installing WooCommerce Square!', 'woocommerce-square' ) } />
+					<SectionDescription>
+						{ __( 'To get started, let’s connect to your Square Account to complete the setup process.', 'woocommerce-square' ) }
+					</SectionDescription>
+
+                    <Button
+                            variant='primary'
+                            href={ '#' }
+                    >
+                        {
+                            __( 'Connect to Square', 'woocommerce-square' )
+                        }
+                    </Button>
+				</div>
+			</div>
+		</div>
+	);
+};
