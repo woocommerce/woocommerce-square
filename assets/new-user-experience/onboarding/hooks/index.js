c720c69278a480ea95df84ec452ba1792ac1bdfe
faisal-alvi <faisalalvi33@gmail.com>
2024-04-15 16:33:54 +0530

    hook `useCashAppData`

--- assets/new-user-experience/onboarding/hooks/index.js
+++ assets/new-user-experience/onboarding/hooks/index.js
@@ -34,6 +34,41 @@ export const useSettings = () => {
 	};
 };
 
+export const useCashAppData = () => {
+	const {
+		getCashAppData,
+		setCashAppData,
+	} = useSettings();
+
+	const [ settingsLoaded, setSettingsLoaded ] = useState( false );
+
+	/**
+	 * Initializes payment gateway data store.
+	 */
+	useEffect( () => {
+		apiFetch( { path: '/wc/v3/wc_square/cash_app_settings' } ).then( ( settings ) => {
+			setCashAppData( {
+				enabled: settings.enabled,
+				title: settings.title,
+				description: settings.description,
+				transaction_type: settings.transaction_type,
+				button_theme: settings.button_theme,
+				button_shape: settings.button_shape,
+			} );
+			setSettingsLoaded( true );
+		} );
+	}, [] );
+
+	const cashApData = {
+		...getCreditCardData(),
+		...getDigitalWalletData(),
+		...getGiftCardData(),
+		...getCashAppData(),
+	};
+
+	return { setCashAppData, cashApData, settingsLoaded };
+};
+
 export const usePaymentGatewayData = () => {
 	const {
 		getCreditCardData,
