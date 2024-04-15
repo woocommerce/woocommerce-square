98516cd14734d916cc40a2ac5588b049c2c0175e
faisal-alvi <faisalalvi33@gmail.com>
2024-04-15 16:34:11 +0530

    include cash app as an onboarding step

--- assets/new-user-experience/onboarding/steps/index.js
+++ assets/new-user-experience/onboarding/steps/index.js
@@ -1,3 +1,4 @@
 export { CreditCardSetup } from './credit-card';
 export { DigitalWalletsSetup } from './digital-wallets';
 export { GiftCardSetup } from './gift-card';
+export { CashAppSetup } from './cash-app';
