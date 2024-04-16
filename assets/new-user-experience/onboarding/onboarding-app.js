9dc6bbd3b3abfd601bf473072475771d0d49cb7b
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2024-04-16 20:56:24 +0530

    push the business location page in the app

--- assets/new-user-experience/onboarding/onboarding-app.js
+++ assets/new-user-experience/onboarding/onboarding-app.js
@@ -22,13 +22,12 @@ export const OnboardingApp = ( { step } ) => {
 	return (
 		<>
 			{ step === 'start' && <ConnectSetup /> }
-			{ step === 'start' && <BusinessLocation /> }
+			{ step === 'business-location' && <BusinessLocation /> }
 			{ step === 'credit-card' && <CreditCardSetup /> }
 			{ step === 'digital-wallets' && <DigitalWalletsSetup /> }
 			{ step === 'gift-card' && <GiftCardSetup /> }
 			{ step === 'cash-app' && <CashAppSetup /> }
-			{/* { step === 'payment-methods' && <PaymentMethods /> } */}
-			{ step === 'payment-methods' && <BusinessLocation /> }
+			{ step === 'payment-methods' && <PaymentMethods /> }
 		</>
 	)
 };
