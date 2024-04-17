37c7b1cc4ff92288b6ac5a9066e8399a3b9777b2
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2024-04-17 13:21:03 +0530

    add payment gateway settings save buttons in onboarding

--- assets/new-user-experience/onboarding/onboarding-app.js
+++ assets/new-user-experience/onboarding/onboarding-app.js
@@ -7,6 +7,7 @@ import { useState } from '@wordpress/element';
  * Internal dependencies.
  */
 import { useSquareSettings } from '../settings/hooks';
+import { usePaymentGatewaySettings } from '../onboarding/hooks';
 import {
 	CashAppSetup,
 	ConnectSetup,
@@ -17,11 +18,27 @@ import {
 	BusinessLocation,
 } from './steps';
 
+import { PaymentGatewaySettingsSaveButton } from '../components';
+
+const paymentGatwaySettingsWithSaveButton = ( WrappedComponent ) => ( props ) => (
+	<>
+		<WrappedComponent { ...props } />
+		<PaymentGatewaySettingsSaveButton />
+	</>
+);
+
+const WrapperCreditCardSetup = paymentGatwaySettingsWithSaveButton( CreditCardSetup );
+const WrapperDigitalWalletsSetup = paymentGatwaySettingsWithSaveButton( DigitalWalletsSetup );
+const WrapperGiftCardSetup = paymentGatwaySettingsWithSaveButton( GiftCardSetup );
+
 export const OnboardingApp = () => {
 	const [step, setStep] = useState('start');
 	const {
 		settings,
 	} = useSquareSettings( true );
+
+	// Calling this once to populate the data store.
+	usePaymentGatewaySettings( true );
 
 	if ( 'start' === step && settings.is_connected ) {
 		setStep('payment-methods');
@@ -32,9 +49,9 @@ export const OnboardingApp = () => {
 			{ step === 'start' && <ConnectSetup /> }
 			{ step === 'business-location' && <BusinessLocation /> }
 			{ step === 'payment-methods' && <PaymentMethods setStep={setStep} /> }
-			{ step === 'credit-card' && <CreditCardSetup /> }
-			{ step === 'digital-wallets' && <DigitalWalletsSetup /> }
-			{ step === 'gift-card' && <GiftCardSetup /> }
+			{ step === 'credit-card' && <WrapperCreditCardSetup /> }
+			{ step === 'digital-wallets' && <WrapperDigitalWalletsSetup /> }
+			{ step === 'gift-card' && <WrapperGiftCardSetup /> }
 			{ step === 'cash-app' && <CashAppSetup /> }
 		</>
 	)
