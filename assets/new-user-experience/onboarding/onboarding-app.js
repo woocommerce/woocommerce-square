1d09113d51fadf26c67fbe211ae7e73e0d7e2478
faisal-alvi <faisalalvi33@gmail.com>
2024-04-24 23:30:18 +0530

    Onboarding App management accomplished

--- assets/new-user-experience/onboarding/onboarding-app.js
+++ assets/new-user-experience/onboarding/onboarding-app.js
@@ -20,19 +20,19 @@ import {
 } from './steps';
 import { ConfigureSync, AdvancedSettings, SandboxSettings } from '../modules';
 
-import { OnboardingHeader, PaymentGatewaySettingsSaveButton, SquareSettingsSaveButton } from '../components';
+import { OnboardingHeader, PaymentGatewaySettingsSaveButton, SquareSettingsSaveButton, Loader } from '../components';
 
 const paymentGatwaySettingsWithSaveButton = ( WrappedComponent ) => ( props ) => (
 	<>
 		<WrappedComponent { ...props } />
-		<PaymentGatewaySettingsSaveButton setStep={props.setStep} nextStep={props.nextStep} />
+		<PaymentGatewaySettingsSaveButton setStep={setStep} nextStep={'payment-complete'} />
 	</>
 );
 
 const squareSettingsWithSaveButton = ( WrappedComponent ) => ( props ) => (
 	<>
 		<WrappedComponent { ...props } />
-		<SquareSettingsSaveButton setStep={props.setStep} nextStep={props.nextStep} />
+		<SquareSettingsSaveButton setStep={setStep} nextStep={'payment-complete'} />
 	</>
 );
 
@@ -47,15 +47,49 @@ const WrapperSandboxSettings = squareSettingsWithSaveButton( SandboxSettings );
 export const OnboardingApp = () => {
 	const [step, setStep] = useState( localStorage.getItem('step') || 'connect-square' );
 	const [backStep, setBackStep] = useState( localStorage.getItem('backStep') || '' );
+	const [settingsLoaded, setSettingsLoaded ] = useState( false );
+	
+	const usePaymentGatewaySettingsData = usePaymentGatewaySettings( true ) ;
+	const {
+		paymentGatewaySettingsLoaded,
+		cashAppGatewaySettingsLoaded,
+		giftCardsGatewaySettingsLoaded
+	} = usePaymentGatewaySettingsData;
+
+	const useSquareSettingsData = useSquareSettings( true );
 	const {
 		settings,
-	} = useSquareSettings( true );
+		squareSettingsLoaded
+	} = useSquareSettingsData;
 
 	// Set info in local storage.
 	useEffect(() => {
 		localStorage.setItem('step', step);
 		localStorage.setItem('backStep', backStep);
 	}, [step, backStep]);
+
+	useEffect(() => {
+		switch (step) {
+			case 'connect-square':
+				setSettingsLoaded(false);
+				break;
+			case 'cash-app':
+				setSettingsLoaded(cashAppGatewaySettingsLoaded);
+				break;
+			case 'gift-card':
+			case 'payment-methods':
+				setSettingsLoaded(giftCardsGatewaySettingsLoaded);
+				break;
+			case 'sync-settings':
+			case 'advanced-settings':
+			case 'sandbox-settings':
+				setSettingsLoaded(squareSettingsLoaded);
+				break;
+			default:
+				setSettingsLoaded(paymentGatewaySettingsLoaded);
+				break;
+		}
+	}, [step, squareSettingsLoaded, paymentGatewaySettingsLoaded, cashAppGatewaySettingsLoaded, giftCardsGatewaySettingsLoaded]);
 
 	// Set the backStep value.
 	useEffect(() => {
@@ -76,28 +110,44 @@ export const OnboardingApp = () => {
 		}
 	}, [step]);
 
-	// Calling this once to populate the data store.
-	usePaymentGatewaySettings( true );
+	if ( ! squareSettingsLoaded ) {
+		return <Loader />;
+	}
 
+	// Redirect to the next page from the connect page when connection is successful.
 	if ( 'connect-square' === step && settings.is_connected ) {
 		setStep('business-location');
+		setSettingsLoaded(true);
+	}
+
+	// Redirect to the connect page when connection is not successful.
+	if ( 'connect-square' !== step && ! settings.is_connected ) {
+		setStep('connect-square');
+		setSettingsLoaded(true);
+	}
+
+	// Set the settings loaded when the connection is not successful on the connection page.
+	if ( 'connect-square' === step && ! settings.is_connected && ! settingsLoaded ) {
+		setSettingsLoaded(true);
 	}
 
 	return (
 		<>
-			<OnboardingHeader backStep={backStep} title={title} setStep={setStep} />
-			<div className="woo-square-onboarding__cover">
-				{ step === 'connect-square' && <ConnectSetup /> }
-				{ step === 'business-location' && <BusinessLocation setStep={setStep} /> }
-				{ step === 'payment-methods' && <PaymentMethods setStep={setStep} /> }
-				{ step === 'payment-complete' && <PaymentComplete setStep={setStep} /> }
-				{ step === 'credit-card' && <WrapperCreditCardSetup setStep={setStep} nextStep={'payment-complete'} /> }
-				{ step === 'digital-wallets' && <WrapperDigitalWalletsSetup setStep={setStep} nextStep={'payment-complete'} /> }
-				{ step === 'gift-card' && <WrapperGiftCardSetup setStep={setStep} nextStep={'payment-complete'} /> }
-				{ step === 'cash-app' && <WrapperCashAppSetup setStep={setStep} nextStep={'payment-complete'} /> }
-				{ step === 'sync-settings' && <WrapperConfigureSyncSetup setStep={setStep} nextStep={'payment-complete'} /> }
-				{ step === 'advanced-settings' && <WrapperAdvancedSettings setStep={setStep} nextStep={'payment-complete'} /> }
-				{ step === 'sandbox-settings' && <WrapperSandboxSettings setStep={setStep} nextStep={'payment-complete'} /> }
+			<OnboardingHeader backStep={backStep} setStep={setStep} />
+			<div className={'woo-square-onboarding__cover ' + step}>
+				{
+					(step === 'connect-square' && <ConnectSetup useSquareSettings={useSquareSettingsData} />) ||
+					(step === 'business-location' && <BusinessLocation setStep={setStep} useSquareSettings={useSquareSettingsData} />) ||
+					(step === 'payment-methods' && <PaymentMethods setStep={setStep} usePaymentGatewaySettings={usePaymentGatewaySettingsData} />) ||
+					(step === 'payment-complete' && <PaymentComplete setStep={setStep} usePaymentGatewaySettings={usePaymentGatewaySettingsData} />) ||
+					(step === 'credit-card' && <WrapperCreditCardSetup usePaymentGatewaySettings={usePaymentGatewaySettingsData} />) ||
+					(step === 'digital-wallets' && <WrapperDigitalWalletsSetup usePaymentGatewaySettings={usePaymentGatewaySettingsData}  />) ||
+					(step === 'gift-card' && <WrapperGiftCardSetup usePaymentGatewaySettings={usePaymentGatewaySettingsData} />) ||
+					(step === 'cash-app' && <WrapperCashAppSetup usePaymentGatewaySettings={usePaymentGatewaySettingsData} />) ||
+					(step === 'sync-settings' && <WrapperConfigureSyncSetup useSquareSettings={useSquareSettingsData} />) ||
+					(step === 'advanced-settings' && <WrapperAdvancedSettings useSquareSettings={useSquareSettingsData} />) ||
+					(step === 'sandbox-settings' && <WrapperSandboxSettings useSquareSettings={useSquareSettingsData} /> )
+				}
 			</div>
 		</>
 	)
