511da256c69a0d2b4b41c10557718798837ce0bb
faisal-alvi <faisalalvi33@gmail.com>
2024-05-01 19:12:04 +0530

    fix the ignore statement

--- includes/Settings.php
+++ includes/Settings.php
@@ -181,7 +181,8 @@ class Settings extends \WC_Settings_API {
 	 * @since 4.7.0
 	 */
 	public function render_square_settings_container() {
-		$section = isset( $_GET['section'] ) && ! empty( $_GET['section'] ) ? wp_unslash( $_GET['section'] ) : 'general'; // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+		// phpcs:ignore WordPress.Security.NonceVerification.Recommended
+		$section = isset( $_GET['section'] ) && ! empty( $_GET['section'] ) ? wp_unslash( $_GET['section'] ) : 'general'; // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
 
 		printf(
 			'<div id="woocommerce-square-settings__container-' . esc_html( $section ) . '"></div>',
@@ -195,8 +196,8 @@ class Settings extends \WC_Settings_API {
 	 */
 	public function render_payments_settings_container() {
 		// phpcs:ignore WordPress.Security.NonceVerification.Recommended
-		$tab     = isset( $_GET['tab'] ) ? wp_unslash( $_GET['tab'] ) : ''; // WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
-		$section = isset( $_GET['section'] ) ? wp_unslash( $_GET['section'] ) : ''; // WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+		$tab     = isset( $_GET['tab'] ) ? wp_unslash( $_GET['tab'] ) : ''; // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+		$section = isset( $_GET['section'] ) ? wp_unslash( $_GET['section'] ) : ''; // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
 
 		if ( 'checkout' !== $tab ) {
 			return;
