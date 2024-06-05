4f6153fc1329662e769082f3333c1b6390de24d9
faisal-alvi <faisalalvi33@gmail.com>
2024-06-06 00:04:16 +0530

    fix modal content size

--- assets/new-user-experience/modules/configure-sync/index.js
+++ assets/new-user-experience/modules/configure-sync/index.js
@@ -265,7 +265,7 @@ export const ConfigureSync = ( { indent = 0, isDirty = false } ) => {
 										<div className='import-modal-cover'>
 											<div className='import-modal-content'>
 												<p>{__( 'You are about to import all new products, variations and categories from Square. This will create a new product in WooCommerce for every product retrieved from Square. If you have products in the trash from the previous imports, these will be ignored in the import.', 'woocommerce-square' )} </p>
-												<h4>{__( 'Do you wish to import existing product updates from Square?', 'woocommerce-square' )} </h4>
+												<h3>{__( 'Do you wish to import existing product updates from Square?', 'woocommerce-square' )} </h3>
 												<p>
 													{
 														parse(
