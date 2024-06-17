af010a6ed7d16e997ebc1919d7f4c31dc4033621
Dharmesh Patel <dspatel44@gmail.com>
2024-06-17 16:28:09 +0530

    Fix eslint errors in Gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,23 @@
 /*jshint node:true */
-module.exports = function( grunt ) {
+module.exports = function (grunt) {
 	'use strict';
-	
+
 	const sass = require('sass');
 
 	const isProduction = process.env.NODE_ENV === 'production';
 	const mode = isProduction ? 'production' : 'development';
 	const makeSourceMaps = mode === 'development';
-	
-	grunt.initConfig({
 
+	grunt.initConfig({
 		// Gets the package vars
-		pkg: grunt.file.readJSON( 'package.json' ),
+		pkg: grunt.file.readJSON('package.json'),
 
 		// Setting folder templates
 		dirs: {
 			css: 'assets/css',
 			fonts: 'assets/fonts',
 			images: 'assets/images',
-			js: 'assets/js'
+			js: 'assets/js',
 		},
 
 		// Compile all .scss files.
@@ -26,32 +25,32 @@ module.exports = function( grunt ) {
 			compile: {
 				options: {
 					sourceMap: makeSourceMaps,
-					implementation: sass
+					implementation: sass,
 				},
-				files: [{
-					expand: true,
-					cwd: '<%= dirs.css %>/',
-					src: ['**/*.scss'],
-					dest: '<%= dirs.css %>/',
-					ext: '.min.css'
-				}]
-			}
+				files: [
+					{
+						expand: true,
+						cwd: '<%= dirs.css %>/',
+						src: ['**/*.scss'],
+						dest: '<%= dirs.css %>/',
+						ext: '.min.css',
+					},
+				],
+			},
 		},
 
 		// Minify all .css files.
 		cssmin: {
 			options: {
-				sourceMap: makeSourceMaps
+				sourceMap: makeSourceMaps,
 			},
 			minify: {
 				expand: true,
 				cwd: '<%= dirs.css %>/',
-				src: [
-					'**/*.css',
-				],
+				src: ['**/*.css'],
 				dest: '<%= dirs.css %>/',
-				ext: '.min.css'
-			}
+				ext: '.min.css',
+			},
 		},
 
 		// Transpile ES6 to ES5
@@ -72,16 +71,16 @@ module.exports = function( grunt ) {
 				],
 			},
 			dist: {
-				files: [{
-					expand: true,
-					cwd: '<%= dirs.js %>/',
-					src: [
-						'**/*.js',
-					],
-					dest: '<%= dirs.js %>/',
-					ext: '.min.js'
-				}]
-			}
+				files: [
+					{
+						expand: true,
+						cwd: '<%= dirs.js %>/',
+						src: ['**/*.js'],
+						dest: '<%= dirs.js %>/',
+						ext: '.min.js',
+					},
+				],
+			},
 		},
 
 		// Watch changes for assets
@@ -89,32 +88,32 @@ module.exports = function( grunt ) {
 			js: {
 				files: [
 					'<%= dirs.js %>/**/*.js',
-					'!<%= dirs.js %>/**/*.min.js'
+					'!<%= dirs.js %>/**/*.min.js',
 				],
-				tasks: ['babel']
+				tasks: ['babel'],
 			},
 			sass: {
 				files: ['<%= dirs.css %>/**/*.scss'],
 				tasks: ['sass', 'cssmin'],
-			}
+			},
 		},
 
 		// Shell scripts
 		shell: {
 			options: {
 				stdout: true,
-				stderr: true
+				stderr: true,
 			},
 			txpull: {
 				command: [
 					'tx pull -a -f', // Transifex download .po files
-				].join( '&&' )
+				].join('&&'),
 			},
 			txpush: {
 				command: [
-					'tx push -s' // Transifex - send .pot file
-				].join( '&&' )
-			}
+					'tx push -s', // Transifex - send .pot file
+				].join('&&'),
+			},
 		},
 
 		// Generate POT files.
@@ -123,25 +122,22 @@ module.exports = function( grunt ) {
 				type: 'wp-plugin',
 				domainPath: 'i18n/languages',
 				potHeaders: {
-					'report-msgid-bugs-to': 'https://wordpress.org/support/plugin/woocommerce-square',
-					'language-team': 'LANGUAGE <EMAIL@ADDRESS>'
-				}
+					'report-msgid-bugs-to':
+						'https://wordpress.org/support/plugin/woocommerce-square',
+					'language-team': 'LANGUAGE <EMAIL@ADDRESS>',
+				},
 			},
 			dist: {
 				options: {
 					potFilename: 'woocommerce-square.pot',
-					exclude: [
-						'apigen/.*',
-						'tests/.*',
-						'tmp/.*'
-					]
-				}
-			}
+					exclude: ['apigen/.*', 'tests/.*', 'tmp/.*'],
+				},
+			},
 		},
 
 		// Check textdomain errors.
 		checktextdomain: {
-			options:{
+			options: {
 				text_domain: 'woocommerce-square',
 				keywords: [
 					'__:1,2d',
@@ -157,57 +153,45 @@ module.exports = function( grunt ) {
 					'_n:1,2,4d',
 					'_nx:1,2,4c,5d',
 					'_n_noop:1,2,3d',
-					'_nx_noop:1,2,3c,4d'
-				]
+					'_nx_noop:1,2,3c,4d',
+				],
 			},
 			files: {
-				src:  [
-					'**/*.php',         // Include all files
-					'!apigen/**',       // Exclude apigen/
+				src: [
+					'**/*.php', // Include all files
+					'!apigen/**', // Exclude apigen/
 					'!node_modules/**', // Exclude node_modules/
-					'!tests/**',        // Exclude tests/
-					'!vendor/**',       // Exclude vendor/
-					'!tmp/**',          // Exclude tmp/
-					'!test-plugins/**'  // Exclude test-plugins/
+					'!tests/**', // Exclude tests/
+					'!vendor/**', // Exclude vendor/
+					'!tmp/**', // Exclude tmp/
+					'!test-plugins/**', // Exclude test-plugins/
 				],
-				expand: true
-			}
+				expand: true,
+			},
 		},
 	});
 
 	// Load NPM tasks to be used here
-	grunt.loadNpmTasks( 'grunt-sass' );
-	grunt.loadNpmTasks( 'grunt-shell' );
-	grunt.loadNpmTasks( 'grunt-phpcs' );
-	grunt.loadNpmTasks( 'grunt-rtlcss' );
-	grunt.loadNpmTasks( 'grunt-postcss' );
-	grunt.loadNpmTasks( 'grunt-stylelint' );
-	grunt.loadNpmTasks( 'grunt-wp-i18n' );
-	grunt.loadNpmTasks( 'grunt-checktextdomain' );
-	grunt.loadNpmTasks( 'grunt-babel' );
-	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
-	grunt.loadNpmTasks( 'grunt-contrib-concat' );
-	grunt.loadNpmTasks( 'grunt-contrib-watch' );
-	grunt.loadNpmTasks( 'grunt-contrib-clean' );
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-shell');
+	grunt.loadNpmTasks('grunt-phpcs');
+	grunt.loadNpmTasks('grunt-rtlcss');
+	grunt.loadNpmTasks('grunt-postcss');
+	grunt.loadNpmTasks('grunt-stylelint');
+	grunt.loadNpmTasks('grunt-wp-i18n');
+	grunt.loadNpmTasks('grunt-checktextdomain');
+	grunt.loadNpmTasks('grunt-babel');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-clean');
 
 	// Register tasks
-	grunt.registerTask( 'default', [
-		'css',
-		'js',
-		'i18n'
-	]);
+	grunt.registerTask('default', ['css', 'js', 'i18n']);
 
-	grunt.registerTask( 'js', [
-		'babel',
-	]);
+	grunt.registerTask('js', ['babel']);
 
-	grunt.registerTask( 'i18n', [
-		'checktextdomain',
-		'makepot'
-	]);
+	grunt.registerTask('i18n', ['checktextdomain', 'makepot']);
 
-	grunt.registerTask( 'css', [
-		'sass',
-		'cssmin'
-	]);
+	grunt.registerTask('css', ['sass', 'cssmin']);
 };
