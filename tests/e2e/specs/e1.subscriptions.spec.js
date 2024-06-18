b33f72b854fa35e3c7f29877372a63d335cba8dd
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2024-06-18 14:52:34 +1000

    Test block and shortcode checkouts.

--- tests/e2e/specs/e1.subscriptions.spec.js
+++ tests/e2e/specs/e1.subscriptions.spec.js
@@ -55,143 +55,144 @@ test.describe('Subscriptions Tests', () => {
 		browser.close();
 	});
 
-	test('Customer can sign up to subscription using Square CreditCard payment gateway', async ({
-		page,
-	}) => {
-		const isBlock = true;
-		await page.goto( '/product/simple-subscription-product' );
-		await page.locator('.single_add_to_cart_button').click();
-		await expect(
-			page.getByRole('link', { name: 'View cart' }).first()
-		).toBeVisible();
-		await visitCheckout(page, isBlock);
-		await fillAddressFields(page, isBlock);
-        await fillCreditCardFields( page, true, isBlock );
-		await placeOrder(page, isBlock);
+	const isBlockCheckout = [ true, false ];
 
-        // verify order received page
-        await expect(
-            page.getByRole('heading', { name: 'Order received' })
-        ).toBeVisible();
-        const orderId = await page
-            .locator('li.woocommerce-order-overview__order strong')
-            .textContent();
+	for ( const isBlock of isBlockCheckout ) {
+		const title = isBlock ? '[Block]: ' : '[non-Block]: ';
 
-		await gotoOrderEditPage(page, orderId);
-		await expect(page.locator('#order_status')).toHaveValue(
-			'wc-processing'
-		);
+		test(title + 'Customer can sign up to subscription using Square CreditCard payment gateway', async ({
+			page,
+		}) => {
+			await page.goto( '/product/simple-subscription-product' );
+			await page.locator('.single_add_to_cart_button').click();
+			await expect(
+				page.getByRole('link', { name: 'View cart' }).first()
+			).toBeVisible();
+			await visitCheckout(page, isBlock);
+			await fillAddressFields(page, isBlock);
+			await fillCreditCardFields( page, true, isBlock );
+			await placeOrder(page, isBlock);
 
-		await page
-			.locator('.woocommerce_subscriptions_related_orders tr td a')
-			.first()
-			.click();
+			// verify order received page
+			await expect(
+				page.getByRole('heading', { name: 'Order received' })
+			).toBeVisible();
+			const orderId = await page
+				.locator('li.woocommerce-order-overview__order strong')
+				.textContent();
 
-		await expect(page.locator('#order_status')).toHaveValue(
-			'wc-active'
-		);
+			await gotoOrderEditPage(page, orderId);
+			await expect(page.locator('#order_status')).toHaveValue(
+				'wc-processing'
+			);
 
-		// Test subscription renewal
-		await renewSubscription(page);
-	});
+			await page
+				.locator('.woocommerce_subscriptions_related_orders tr td a')
+				.first()
+				.click();
 
-	test('Customer can sign up to subscription using Saved CreditCard', async ({
-		page,
-	}) => {
-		const isBlock = true;
-		await page.goto( '/product/simple-subscription-product' );
-		await page.locator('.single_add_to_cart_button').click();
-		await expect(
-			page.getByRole('link', { name: 'View cart' }).first()
-		).toBeVisible();
-		await visitCheckout(page, isBlock);
+			await expect(page.locator('#order_status')).toHaveValue(
+				'wc-active'
+			);
 
-		await page
-			.locator( '.wc-block-checkout__payment-method .wc-block-components-radio-control' )
-			.locator( 'input.wc-block-components-radio-control__input' )
-			.first()
-			.check();
-		await placeOrder(page, isBlock);
+			// Test subscription renewal
+			await renewSubscription(page);
+		});
 
-        // verify order received page
-        await expect(
-            page.getByRole('heading', { name: 'Order received' })
-        ).toBeVisible();
-        const orderId = await page
-            .locator('li.woocommerce-order-overview__order strong')
-            .textContent();
+		test(title + 'Customer can sign up to subscription using Saved CreditCard', async ({
+			page,
+		}) => {
+			await page.goto( '/product/simple-subscription-product' );
+			await page.locator('.single_add_to_cart_button').click();
+			await expect(
+				page.getByRole('link', { name: 'View cart' }).first()
+			).toBeVisible();
+			await visitCheckout(page, isBlock);
 
-		await gotoOrderEditPage(page, orderId);
-		await expect(page.locator('#order_status')).toHaveValue(
-			'wc-processing'
-		);
+			await page
+				.locator( '.wc-block-checkout__payment-method .wc-block-components-radio-control' )
+				.locator( 'input.wc-block-components-radio-control__input' )
+				.first()
+				.check();
+			await placeOrder(page, isBlock);
 
-		await page
-			.locator('.woocommerce_subscriptions_related_orders tr td a')
-			.first()
-			.click();
+			// verify order received page
+			await expect(
+				page.getByRole('heading', { name: 'Order received' })
+			).toBeVisible();
+			const orderId = await page
+				.locator('li.woocommerce-order-overview__order strong')
+				.textContent();
 
-		await expect(page.locator('#order_status')).toHaveValue(
-			'wc-active'
-		);
+			await gotoOrderEditPage(page, orderId);
+			await expect(page.locator('#order_status')).toHaveValue(
+				'wc-processing'
+			);
 
-		// Test subscription renewal
-		await renewSubscription(page);
-	});
+			await page
+				.locator('.woocommerce_subscriptions_related_orders tr td a')
+				.first()
+				.click();
 
-	test('Customer can early renew the subscription using Square CreditCard', async ({
-		page,
-	}) => {
-		const isBlock = true;
-		await page.goto( '/my-account/subscriptions/' );
-		const subscriptionId = await page.locator('table.my_account_subscriptions tr td.subscription-id a').first().textContent();
-		await page.locator('table.my_account_subscriptions tr td.subscription-actions a').first().click();
+			await expect(page.locator('#order_status')).toHaveValue(
+				'wc-active'
+			);
 
-		await page.locator('table.subscription_details a.subscription_renewal_early').first().click();
-		await page
-			.locator( '.wc-block-checkout__payment-method .wc-block-components-radio-control' )
-			.locator( 'input.wc-block-components-radio-control__input' )
-			.first()
-			.check();
-		await placeOrder(page, isBlock);
+			// Test subscription renewal
+			await renewSubscription(page);
+		});
 
-        // verify order received page
-		await expect(
-			page.getByRole('heading', { name: 'Order received' })
-		).toBeVisible();
+		test(title + 'Customer can early renew the subscription using Square CreditCard', async ({
+			page,
+		}) => {
+			await page.goto( '/my-account/subscriptions/' );
+			const subscriptionId = await page.locator('table.my_account_subscriptions tr td.subscription-id a').first().textContent();
+			await page.locator('table.my_account_subscriptions tr td.subscription-actions a').first().click();
 
-		await page.goto('my-account/view-subscription/'+subscriptionId.replace('#', ''));
-		await expect(page.locator('table.subscription_details tr').first().locator('td').last()).toHaveText('Active');
-	});
+			await page.locator('table.subscription_details a.subscription_renewal_early').first().click();
+			await page
+				.locator( '.wc-block-checkout__payment-method .wc-block-components-radio-control' )
+				.locator( 'input.wc-block-components-radio-control__input' )
+				.first()
+				.check();
+			await placeOrder(page, isBlock);
 
-	test('Customer can change payment method of the subscription', async ({
-		page,
-	}) => {
-		const isBlock = false;
-		await page.goto( '/my-account/subscriptions/' );
-		await page.locator('table.my_account_subscriptions tr td.subscription-actions a').first().click();
+			// verify order received page
+			await expect(
+				page.getByRole('heading', { name: 'Order received' })
+			).toBeVisible();
 
-		await page.locator('table.subscription_details a.change_payment_method').first().click();
-		await page.locator('input[name="wc-square-credit-card-payment-token"]').first().waitFor( { state: 'visible' } );
-		if( await page.locator('input#wc-square-credit-card-use-new-payment-method').isVisible() ){
-			await page.locator('input#wc-square-credit-card-use-new-payment-method').check();
-		}
-		await fillCreditCardFields( page, true, isBlock );
-		await placeOrder(page, isBlock);
+			await page.goto('my-account/view-subscription/'+subscriptionId.replace('#', ''));
+			await expect(page.locator('table.subscription_details tr').first().locator('td').last()).toHaveText('Active');
+		});
 
-        // verify order received page
-		await expect(
-			await page.locator('.woocommerce .woocommerce-message').first()
-		).toBeVisible();
-		await expect(
-			await page.locator('.woocommerce .woocommerce-message').first()
-		).toHaveText(/Payment method updated/);
-	});
+		test(title + 'Customer can change payment method of the subscription', async ({
+			page,
+		}) => {
+			await page.goto( '/my-account/subscriptions/' );
+			await page.locator('table.my_account_subscriptions tr td.subscription-actions a').first().click();
+
+			await page.locator('table.subscription_details a.change_payment_method').first().click();
+			await page.locator('input[name="wc-square-credit-card-payment-token"]').first().waitFor( { state: 'visible' } );
+			if( await page.locator('input#wc-square-credit-card-use-new-payment-method').isVisible() ){
+				await page.locator('input#wc-square-credit-card-use-new-payment-method').check();
+			}
+			await fillCreditCardFields( page, true, isBlock );
+			await placeOrder(page, isBlock);
+
+			// verify order received page
+			await expect(
+				await page.locator('.woocommerce .woocommerce-message').first()
+			).toBeVisible();
+			await expect(
+				await page.locator('.woocommerce .woocommerce-message').first()
+			).toHaveText(/Payment method updated/);
+		});
+	}
 
 	test('Customer can cancel the subscription', async ({
 		page,
 	}) => {
-		const isBlock = false;
 		await page.goto( '/my-account/subscriptions/' );
 		await page.locator('table.my_account_subscriptions tr td.subscription-actions a').first().click();
 
