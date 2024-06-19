1ae71bd6393793483a1b21c544a7c5b4f177db20
Dharmesh Patel <dspatel44@gmail.com>
2024-06-19 18:25:09 +0530

    Move rewrite structure to last.

--- tests/e2e/config/env-post-setup.sh
+++ tests/e2e/config/env-post-setup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # Set permalink structure.
-wp-env run tests-wordpress chmod -c ugo+w /var/www/html
 wp-env run tests-cli wp post create --post_type=page --post_content='<!-- wp:shortcode -->[woocommerce_checkout]<!-- /wp:shortcode -->' --post_title="Checkout Old" --post_status=publish
-wp-env run tests-cli wp rewrite structure '/%postname%/' --hard
 
 # Activate the storefront theme.
 wp-env run tests-cli wp theme activate storefront
@@ -14,3 +12,6 @@ wp-env run tests-cli wp transient delete --all
 
 # Create subscription product.
 wp-env run tests-cli wp wc product create -- --name="Simple Subscription Product" --slug="simple-subscription-product" --user=1 --regular_price=10 --type=subscription --meta_data='[{"key":"_subscription_price","value":"10"},{"key":"_subscription_period","value":"month"},{"key":"_subscription_period_interval","value":"1"}]'
+
+wp-env run tests-wordpress chmod -c ugo+w /var/www/html
+wp-env run tests-cli wp rewrite structure '/%postname%/' --hard
