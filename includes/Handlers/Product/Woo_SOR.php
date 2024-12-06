4256504ec692057983ede615aa2d5196a55d2c58
faisal-alvi <faisalalvi33@gmail.com>
2024-12-06 17:55:48 +0530

    fix issue of 0 option values expected, found 3

--- includes/Handlers/Product/Woo_SOR.php
+++ includes/Handlers/Product/Woo_SOR.php
@@ -291,7 +291,8 @@ class Woo_SOR extends \WooCommerce\Square\Handlers\Product {
 		 */
 		if ( 'variation' === $product->get_type() ) {
 			$options_data_transient = get_transient( 'wc_square_options_data' );
-			$variation_items        = $product->get_attributes();
+			$parent_product         = wc_get_product( $product->get_parent_id() );
+			$variation_items        = $parent_product->get_attributes();
 			$variation_item_values  = array();
 
 			if ( 1 === count( $variation_items ) ) {
@@ -355,7 +356,6 @@ class Woo_SOR extends \WooCommerce\Square\Handlers\Product {
 							$attribute_option_values = wp_list_pluck( $attribute_option_values, 'name' );
 						} else {
 							// Get all attribute values from the parent product.
-							$parent_product          = wc_get_product( $product->get_parent_id() );
 							$attribute_option_values = $parent_product->get_attribute( $attribute_id );
 							$attribute_option_values = array_map( 'trim', explode( '|', $attribute_option_values ) );
 						}
