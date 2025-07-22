ca32dda4170fc7762f130bd17237e5a3ee8dbe29
Dharmesh Patel <dspatel44@gmail.com>
2025-07-22 17:01:31 +0530

    E2E: Add SCA card testing for existing E2E tests.

--- tests/e2e/specs/a7.transaction-authorize-virtual-only.spec.js
+++ tests/e2e/specs/a7.transaction-authorize-virtual-only.spec.js
@@ -44,77 +44,91 @@ test.beforeAll( 'Setup', async ( { baseURL } ) => {
 		await page.locator( '#_virtual' ).check();
 		await page.waitForTimeout( 2000 );
 		await page.locator( '#publish' ).click();
-		await expect( await page.getByText( 'Product published' ) ).toBeVisible();
+		await expect( page.getByText( 'Product published' ) ).toBeVisible();
 	}
 
 	await clearCart( page );
 	await browser.close();
 } );
 
-test( 'Payment Gateway > Transaction Type > Authorization + Virtual Only @general', async ( {
-	page,
-} ) => {
-	await addOneOrMoreProductToCart( page, 'virtual-product' );
+for ( const isSCA of [ true, false ] ) {
+	const title = isSCA ? ' [SCA]:' : ' ';
+	test(
+		title +
+			'Payment Gateway > Transaction Type > Authorization + Virtual Only @general',
+		async ( { page } ) => {
+			await page.goto(
+				'/wp-admin/admin.php?page=wc-settings&tab=checkout&section=square_credit_card'
+			);
+			await page
+				.getByTestId( 'credit-card-gateway-virtual-order-only-field' )
+				.uncheck();
+			await savePaymentGatewaySettings( page );
+			await addOneOrMoreProductToCart( page, 'virtual-product' );
 
-	await visitCheckout( page, false );
-	await fillAddressFields( page, false );
-	await fillCreditCardFields( page, true, false );
-	await placeOrder( page, false );
+			await visitCheckout( page, false );
+			await fillAddressFields( page, false );
+			await fillCreditCardFields( page, true, false, isSCA );
+			await placeOrder( page, false, isSCA );
 
-	await expect(
-		page.locator( '.woocommerce-order-overview__total strong' )
-	).toHaveText( '$7.99' );
-	const orderId = await page
-		.locator( '.woocommerce-order-overview__order strong' )
-		.innerText();
+			await expect(
+				page.locator( '.woocommerce-order-overview__total strong' )
+			).toHaveText( '$7.99' );
+			const orderId = await page
+				.locator( '.woocommerce-order-overview__order strong' )
+				.innerText();
 
-	await gotoOrderEditPage( page, orderId );
+			await gotoOrderEditPage( page, orderId );
 
-	await expect( page.locator( '#order_status' ) ).toHaveValue(
-		'wc-on-hold'
+			await expect( page.locator( '#order_status' ) ).toHaveValue(
+				'wc-on-hold'
+			);
+			await expect(
+				page.getByText(
+					'Square Test Authorization Approved for an amount of $7.99: Visa ending in '
+				)
+			).toBeVisible();
+		}
 	);
-	await expect(
-		page.getByText(
-			'Square Test Authorization Approved for an amount of $7.99: Visa ending in 1111'
-		)
-	).toBeVisible();
-} );
 
-test( 'Payment Gateway > Transaction Type > Authorization + Virtual Only but charge @general', async ( {
-	page,
-} ) => {
-	await page.goto(
-		'/wp-admin/admin.php?page=wc-settings&tab=checkout&section=square_credit_card'
+	test(
+		title +
+			'Payment Gateway > Transaction Type > Authorization + Virtual Only but charge @general',
+		async ( { page } ) => {
+			await page.goto(
+				'/wp-admin/admin.php?page=wc-settings&tab=checkout&section=square_credit_card'
+			);
+
+			await page
+				.getByTestId( 'credit-card-gateway-virtual-order-only-field' )
+				.check();
+
+			await savePaymentGatewaySettings( page );
+
+			await addOneOrMoreProductToCart( page, 'virtual-product' );
+
+			await visitCheckout( page, false );
+			await fillAddressFields( page, false );
+			await fillCreditCardFields( page, null, false, isSCA );
+			await placeOrder( page, false, isSCA );
+
+			await expect(
+				page.locator( '.woocommerce-order-overview__total strong' )
+			).toHaveText( '$7.99' );
+			const orderId = await page
+				.locator( '.woocommerce-order-overview__order strong' )
+				.innerText();
+
+			await gotoOrderEditPage( page, orderId );
+
+			await expect( page.locator( '#order_status' ) ).toHaveValue(
+				'wc-processing'
+			);
+			await expect(
+				page.getByText(
+					'Square Test Charge Approved for an amount of $7.99: Visa ending in '
+				)
+			).toBeVisible();
+		}
 	);
-
-	await page
-		.getByTestId( 'credit-card-gateway-virtual-order-only-field' )
-		.check();
-
-	await savePaymentGatewaySettings( page );
-
-	await addOneOrMoreProductToCart( page, 'virtual-product' );
-
-	await visitCheckout( page, false );
-	await fillAddressFields( page, false );
-	await fillCreditCardFields( page, null, false );
-	await placeOrder( page, false );
-
-	await expect(
-		page.locator( '.woocommerce-order-overview__total strong' )
-	).toHaveText( '$7.99' );
-	const orderId = await page
-		.locator( '.woocommerce-order-overview__order strong' )
-		.innerText();
-
-	await gotoOrderEditPage( page, orderId );
-
-	await expect( page.locator( '#order_status' ) ).toHaveValue(
-		'wc-processing'
-	);
-	await expect(
-		page.getByText(
-			'Square Test Charge Approved for an amount of $7.99: Visa ending in 1111'
-		)
-	).toBeVisible();
-} );
+}
--- tests/e2e/specs/f1.pre-orders.spec.js
+++ tests/e2e/specs/f1.pre-orders.spec.js
@@ -1,18 +1,18 @@
 /**
  * External dependencies
  */
-const { test, expect, devices, chromium } = require('@playwright/test');
+const { test, expect, devices, chromium } = require( '@playwright/test' );
 
 /**
  * Internal dependencies
  */
 import {
-    createPreOrderProduct,
+	createPreOrderProduct,
 	fillAddressFields,
-    fillCreditCardFields,
-    placeOrder,
+	fillCreditCardFields,
+	placeOrder,
 	visitCheckout,
-    completePreOrder,
+	completePreOrder,
 	saveCashAppPaySettings,
 	gotoOrderEditPage,
 	selectPaymentMethod,
@@ -20,12 +20,11 @@ import {
 	savePaymentGatewaySettings,
 	runWpCliCommand,
 } from '../utils/helper';
-const iPhone = devices['iPhone 14 Pro Max'];
+const iPhone = devices[ 'iPhone 14 Pro Max' ];
 
-test.describe('Pre-Orders Tests @general', () => {
-	test.slow();
+test.describe( 'Pre-Orders Tests @general', () => {
 
-	test.beforeAll('Setup', async () => {
+	test.beforeAll( 'Setup', async () => {
 		const browser = await chromium.launch();
 		const page = await browser.newPage();
 
@@ -35,163 +34,222 @@ test.describe('Pre-Orders Tests @general', () => {
 		await page
 			.getByTestId( 'credit-card-transaction-type-field' )
 			.selectOption( { label: 'Charge' } );
-		await page
-			.getByTestId( 'credit-card-tokenization-field' )
-			.check();
+		await page.getByTestId( 'credit-card-tokenization-field' ).check();
 		await savePaymentGatewaySettings( page );
 
 		// Set authorization transaction type.
-		await saveCashAppPaySettings(page, {
+		await saveCashAppPaySettings( page, {
 			transactionType: 'charge',
-		});
+		} );
 
 		await runWpCliCommand(
 			'wp option update woocommerce_feature_product_block_editor_enabled "no"'
 		);
-	});
+	} );
 
-	test('[Charge upon release] Square Credit Card should work with Pre-Orders', async ({
-        page,
-	}) => {
-        const isBlock = true;
-		const productId = await createPreOrderProduct(page, {
-			whenToCharge: 'upon_release',
-		});
-		await page.goto('/?p=' + productId);
-		await page.locator('.single_add_to_cart_button').click();
-		await expect(
-			page.getByRole('link', { name: 'View cart' }).first()
-		).toBeVisible();
-		await visitCheckout(page, isBlock);
-        await fillAddressFields(page, isBlock);
-		if ( await page.locator( 'span[aria-label="Use another payment method"][aria-expanded="false"]' ).isVisible() ) {
-			await page.locator( 'span[aria-label="Use another payment method"][aria-expanded="false"]' ).click();
+	const isBlockCheckout = [ true, false ];
+	for ( const isBlock of isBlockCheckout ) {
+		const title = isBlock ? '[Block]:' : '[non-Block]:';
+		for ( const isSCA of [ true, false ] ) {
+			const subTitle = isSCA ? ' [SCA]:' : ' ';
+			test(
+				title +
+					subTitle +
+					'[Charge upon release] Square Credit Card should work with Pre-Orders',
+				async ( { page } ) => {
+					if ( ! isBlock ) {
+						// Skipping block checkout tests due to multiple iframe issue. ToDo: handle this later.
+						test.skip();
+					}
+					const productId = await createPreOrderProduct( page, {
+						whenToCharge: 'upon_release',
+					} );
+					await page.goto( '/?p=' + productId );
+					await page.locator( '.single_add_to_cart_button' ).click();
+					await expect(
+						page.getByRole( 'link', { name: 'View cart' } ).first()
+					).toBeVisible();
+					await visitCheckout( page, isBlock );
+					await fillAddressFields( page, isBlock );
+					if (
+						await page
+							.locator(
+								'span[aria-label="Use another payment method"][aria-expanded="false"]'
+							)
+							.isVisible()
+					) {
+						await page
+							.locator(
+								'span[aria-label="Use another payment method"][aria-expanded="false"]'
+							)
+							.click();
+					}
+					await fillCreditCardFields( page, true, isBlock, isSCA );
+					await placeOrder( page, isBlock, isSCA );
+
+					// verify order received page
+					await expect(
+						page.getByRole( 'heading', { name: 'Order received' } )
+					).toBeVisible();
+					const orderId = await page
+						.locator(
+							'li.woocommerce-order-overview__order strong'
+						)
+						.textContent();
+
+					// Verify order status is Pre-Ordered.
+					await page.goto(
+						`/wp-admin/post.php?post=${ orderId }&action=edit`
+					);
+					const orderStatus = await page.locator(
+						'select[name="order_status"]'
+					);
+					await expect(
+						await orderStatus.evaluate( ( el ) => el.value )
+					).toBe( 'wc-pre-ordered' );
+
+					// Complete pre-order.
+					await completePreOrder( page, orderId );
+
+					// Verify order status is Processing.
+					await page.goto(
+						`/wp-admin/post.php?post=${ orderId }&action=edit`
+					);
+					await expect(
+						await page
+							.locator( '#order_status' )
+							.evaluate( ( el ) => el.value )
+					).toEqual( 'wc-processing' );
+
+					// Verify order note is added.
+					await expect(
+						page
+							.locator(
+								'#woocommerce-order-notes ul.order_notes li',
+								{
+									hasText:
+										'Pre-Order Release Payment Approved:',
+								}
+							)
+							.first()
+					).toBeVisible();
+				}
+			);
+
+			test(
+				title +
+					subTitle +
+					'[Upfront Charge] Square Credit Card should work with Pre-Orders',
+				async ( { page } ) => {
+					const productId = await createPreOrderProduct( page, {
+						whenToCharge: 'upfront',
+					} );
+					await page.goto( '/?p=' + productId );
+					await page.locator( '.single_add_to_cart_button' ).click();
+					await expect(
+						page.getByRole( 'link', { name: 'View cart' } ).first()
+					).toBeVisible();
+					await visitCheckout( page, isBlock );
+					await fillAddressFields( page, isBlock );
+					if (
+						await page
+							.locator(
+								'span[aria-label="Use another payment method"][aria-expanded="false"]'
+							)
+							.isVisible()
+					) {
+						await page
+							.locator(
+								'span[aria-label="Use another payment method"][aria-expanded="false"]'
+							)
+							.click();
+					}
+					await fillCreditCardFields( page, true, isBlock, isSCA );
+					await placeOrder( page, isBlock, isSCA );
+
+					// verify order received page
+					await expect(
+						page.getByRole( 'heading', { name: 'Order received' } )
+					).toBeVisible();
+					const orderId = await page
+						.locator(
+							'li.woocommerce-order-overview__order strong'
+						)
+						.textContent();
+
+					// Verify order status is Pre-Ordered.
+					await page.goto(
+						`/wp-admin/post.php?post=${ orderId }&action=edit`
+					);
+					const orderStatus = await page
+						.locator( 'select[name="order_status"]' )
+						.evaluate( ( el ) => el.value );
+					await expect( orderStatus ).toBe( 'wc-pre-ordered' );
+
+					// Complete pre-order.
+					await completePreOrder( page, orderId );
+
+					// Verify order status is processing.
+					await page.goto(
+						`/wp-admin/post.php?post=${ orderId }&action=edit`
+					);
+					await expect(
+						await page
+							.locator( '#order_status' )
+							.evaluate( ( el ) => el.value )
+					).toEqual( 'wc-processing' );
+				}
+			);
 		}
-        await fillCreditCardFields( page, true, isBlock );
-		await placeOrder(page, isBlock);
+	}
 
-        // verify order received page
-        await expect(
-            page.getByRole('heading', { name: 'Order received' })
-        ).toBeVisible();
-        const orderId = await page
-            .locator('li.woocommerce-order-overview__order strong')
-            .textContent();
-
-		// Verify order status is Pre-Ordered.
-		await page.goto(`/wp-admin/post.php?post=${orderId}&action=edit`);
-		const orderStatus = await page.locator(
-			'select[name="order_status"]'
-		);
-		await expect(await orderStatus.evaluate((el) => el.value)).toBe(
-			'wc-pre-ordered'
-		);
-
-        // Complete pre-order.
-		await completePreOrder(page, orderId);
-
-		// Verify order status is Processing.
-		await page.goto(`/wp-admin/post.php?post=${orderId}&action=edit`);
-		await expect(
-			await page.locator('#order_status').evaluate((el) => el.value)
-		).toEqual('wc-processing');
-
-        // Verify order note is added.
-        await expect(
-			page
-				.locator('#woocommerce-order-notes ul.order_notes li', {
-					hasText: 'Pre-Order Release Payment Approved:',
-				})
-				.first()
-		).toBeVisible();
-	});
-
-	test('[Upfront Charge] Square Credit Card should work with Pre-Orders', async ({
-		page,
-	}) => {
-        const isBlock = true;
-		const productId = await createPreOrderProduct(page, {
-			whenToCharge: 'upfront',
-		});
-		await page.goto('/?p=' + productId);
-		await page.locator('.single_add_to_cart_button').click();
-		await expect(
-			page.getByRole('link', { name: 'View cart' }).first()
-		).toBeVisible();
-		await visitCheckout(page, isBlock);
-        await fillAddressFields(page, isBlock);
-		if ( await page.locator( 'span[aria-label="Use another payment method"][aria-expanded="false"]' ).isVisible() ) {
-			await page.locator( 'span[aria-label="Use another payment method"][aria-expanded="false"]' ).click();
-		}
-        await fillCreditCardFields( page, true, isBlock );
-		await placeOrder(page, isBlock);
-
-        // verify order received page
-        await expect(
-            page.getByRole('heading', { name: 'Order received' })
-        ).toBeVisible();
-        const orderId = await page
-            .locator('li.woocommerce-order-overview__order strong')
-            .textContent();
-
-
-
-		// Verify order status is Pre-Ordered.
-		await page.goto(
-            `/wp-admin/post.php?post=${orderId}&action=edit`
-        );
-		const orderStatus = await page
-			.locator('select[name="order_status"]')
-			.evaluate((el) => el.value);
-		await expect(orderStatus).toBe('wc-pre-ordered');
-
-		// Complete pre-order.
-		await completePreOrder(page, orderId);
-
-		// Verify order status is processing.
-		await page.goto(`/wp-admin/post.php?post=${orderId}&action=edit`);
-		await expect(
-			await page.locator('#order_status').evaluate((el) => el.value)
-		).toEqual('wc-processing');
-	});
-
-	test('[Upfront Charge] Square Cash App Pay should work with Pre-Orders @cashapp', async ({
+	test( '[Upfront Charge] Square Cash App Pay should work with Pre-Orders @cashapp', async ( {
 		browser,
 		page: adminPage,
-	}) => {
-		const context = await browser.newContext({
+	} ) => {
+		const context = await browser.newContext( {
 			...iPhone,
-		});
+		} );
 		const page = await context.newPage();
-        const isBlock = true;
-		const productId = await createPreOrderProduct(page, {
+		const isBlock = true;
+		const productId = await createPreOrderProduct( page, {
 			whenToCharge: 'upfront',
-		});
-		await page.goto('/?p=' + productId);
-		await page.locator('.single_add_to_cart_button').click();
+		} );
+		await page.goto( '/?p=' + productId );
+		await page.locator( '.single_add_to_cart_button' ).click();
 		await expect(
-			page.getByRole('link', { name: 'View cart' }).first()
+			page.getByRole( 'link', { name: 'View cart' } ).first()
 		).toBeVisible();
-		await visitCheckout(page, isBlock);
-		await fillAddressFields(page, isBlock);
-		if ( await page.locator( 'span[aria-label="Use another payment method"][aria-expanded="false"]' ).isVisible() ) {
-			await page.locator( 'span[aria-label="Use another payment method"][aria-expanded="false"]' ).click();
+		await visitCheckout( page, isBlock );
+		await fillAddressFields( page, isBlock );
+		if (
+			await page
+				.locator(
+					'span[aria-label="Use another payment method"][aria-expanded="false"]'
+				)
+				.isVisible()
+		) {
+			await page
+				.locator(
+					'span[aria-label="Use another payment method"][aria-expanded="false"]'
+				)
+				.click();
 		}
-		await selectPaymentMethod(page, 'square_cash_app_pay', isBlock);
-		const orderId = await placeCashAppPayOrder(page, isBlock);
+		await selectPaymentMethod( page, 'square_cash_app_pay', isBlock );
+		const orderId = await placeCashAppPayOrder( page, isBlock );
 
-		await gotoOrderEditPage(adminPage, orderId);
-		await expect(adminPage.locator('#order_status')).toHaveValue(
+		await gotoOrderEditPage( adminPage, orderId );
+		await expect( adminPage.locator( '#order_status' ) ).toHaveValue(
 			'wc-pre-ordered'
 		);
 
 		// Complete pre-order.
-		await completePreOrder(adminPage, orderId);
+		await completePreOrder( adminPage, orderId );
 
 		// Verify order status is processing.
-		await gotoOrderEditPage(adminPage, orderId);
-		await expect(adminPage.locator('#order_status')).toHaveValue(
+		await gotoOrderEditPage( adminPage, orderId );
+		await expect( adminPage.locator( '#order_status' ) ).toHaveValue(
 			'wc-processing'
 		);
-	});
-});
+	} );
+} );
