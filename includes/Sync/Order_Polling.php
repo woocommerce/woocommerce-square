feab47a994ebcd61276c2df42c678eeb002853fd
faisal-alvi <faisalalvi33@gmail.com>
2025-07-24 23:02:19 +0530

    Add custom cron schedule for Square order polling

--- includes/Sync/Order_Polling.php
+++ includes/Sync/Order_Polling.php
@@ -33,6 +33,46 @@ class Order_Polling {
 	 * @since x.x.x
 	 */
 	public function __construct() {
-		
+		// Add filter to add custom cron schedule.
+		add_filter( 'cron_schedules', array( $this, 'add_custom_cron_schedule' ) );
+	}
+
+	/**
+	 * Add custom cron schedule for Square order polling.
+	 *
+	 * @since x.x.x
+	 * @param array $schedules Existing cron schedules.
+	 * @return array Modified cron schedules.
+	 */
+	public function add_custom_cron_schedule( $schedules ) {
+		$polling_interval = $this->get_polling_interval_seconds();
+
+		$schedules['square_order_polling'] = array(
+			'interval' => $polling_interval,
+			'display'  => sprintf(
+				/* translators: %d: number of minutes */
+				__( 'Every %d minutes (Square Order Polling)', 'woocommerce-square' ),
+				round( $polling_interval / MINUTE_IN_SECONDS )
+			),
+		);
+
+		return $schedules;
+	}
+
+	/**
+	 * Get polling interval in seconds.
+	 *
+	 * @since x.x.x
+	 * @return int
+	 */
+	private function get_polling_interval_seconds() {
+		/**
+		 * Filters the polling interval in seconds for Square orders.
+		 *
+		 * @since x.x.x
+		 * @param int $interval The polling interval in seconds.
+		 * @return int The polling interval in seconds.
+		 */
+		return apply_filters( 'wc_square_order_polling_interval_seconds', 15 * MINUTE_IN_SECONDS );
 	}
 }
