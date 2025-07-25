f0fe8fc56ba967c50078bfa6499dba539952882b
faisal-alvi <faisalalvi33@gmail.com>
2025-07-25 17:40:02 +0530

    Remove unused "future" method `maybe_sync_update_to_square()` for now

--- includes/Handlers/Order_Sync.php
+++ includes/Handlers/Order_Sync.php
@@ -30,21 +30,5 @@ class Order_Sync {
 	public static function init() {
 		// Initialize order polling system.
 		new Order_Polling();
-
-		// Sync updated WooCommerce order to Square.
-		add_action( 'woocommerce_update_order', array( __CLASS__, 'maybe_sync_update_to_square' ), 10, 2 );
-	}
-
-	/**
-	 * Sync updated WooCommerce order to Square if linked.
-	 *
-	 * @since x.x.x
-	 *
-	 * @param int       $order_id WooCommerce order ID.
-	 * @param \WC_Order $order WooCommerce order object.
-	 */
-	public static function maybe_sync_update_to_square( $order_id, $order ) {
-		// @TODO: For future updates.
-		return;
 	}
 }
