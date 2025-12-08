6050071d1fa3175acb81b825f39843367802316a
Peter Wilson <git-commit-by@peterwilson.cc>
2025-12-08 13:22:30 +1100

    Revert minimum WP version bump.

--- woocommerce-square.php
+++ woocommerce-square.php
@@ -4,7 +4,7 @@
  * Requires Plugins: woocommerce
  * Version: 5.1.1
  * Plugin URI: https://woocommerce.com/products/square/
- * Requires at least: 6.7
+ * Requires at least: 6.5
  * Tested up to: 6.9
  * Requires PHP: 7.4
  * PHP tested up to: 8.4
