47730bef1ccd11f732bd2210d5c36808970b89db
Peter Wilson <git-commit-by@peterwilson.cc>
2025-12-08 13:22:42 +1100

    Revert minimum WC version bump.

--- woocommerce-square.php
+++ woocommerce-square.php
@@ -22,7 +22,7 @@
  * @copyright Copyright (c) 2019, Automattic, Inc.
  * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0 or later
  *
- * WC requires at least: 10.2
+ * WC requires at least: 10.1
  * WC tested up to: 10.4
  */
 
@@ -63,7 +63,7 @@ class WooCommerce_Square_Loader {
 	const MINIMUM_WP_VERSION = '6.7';
 
 	/** minimum WooCommerce version required by this plugin */
-	const MINIMUM_WC_VERSION = '10.2';
+	const MINIMUM_WC_VERSION = '10.1';
 
 	/**
 	 * SkyVerge plugin framework version used by this plugin
