f52064487c6c337765ec05f0bb38e21e978767c0
Dharmesh Patel <dspatel44@gmail.com>
2026-01-27 18:39:18 +0530

    Ensure that there is no `DivisionByZeroError` fatal error in Square when the total amount is zero and the tax amount is non-zero.

--- includes/Gateway/API/Requests/Orders.php
+++ includes/Gateway/API/Requests/Orders.php
@@ -443,8 +443,15 @@ class Orders extends API\Request {
 					$item_uid            = $item->get_id();
 					$tax_uid             = $taxes[ $key ]->getUid();
 					$prev_percentage     = $taxes[ $key ]->getPercentage();
-					$adjusted_percentage = (float) $tax_amount * 100 / $total_amount;
-					$adjusted_percentage = number_format( (float) $adjusted_percentage, 2, '.', '' );
+					$adjusted_percentage = $prev_percentage;
+					/*
+					 * $total_amount could be 0 for some cases, eg: Retail Delivery Fee via Avalara AvaTax for Minnesota (MN) and Colorado (CO).
+					 * @see https://linear.app/a8c/issue/SQUARE-232/divisionbyzeroerror-in-square-gateway-with-zero-amount-fee-tax
+					 */
+					if ( ! empty( $total_amount ) ) {
+						$adjusted_percentage = (float) $tax_amount * 100 / $total_amount;
+						$adjusted_percentage = number_format( (float) $adjusted_percentage, 2, '.', '' );
+					}
 
 					if ( $prev_percentage !== $adjusted_percentage ) {
 						// Create a new tax.
