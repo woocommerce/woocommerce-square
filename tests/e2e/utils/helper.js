3ee94e70c75ea92ab3c7626676fcf9054a3c49f1
Dharmesh Patel <dspatel44@gmail.com>
2026-08-12 18:07:15 +0530

    E2E: Update selector for post ID in quick edit to accommodate WP 7.1 markup changes.

--- tests/e2e/utils/helper.js
+++ tests/e2e/utils/helper.js
@@ -684,7 +684,7 @@ export async function completePreOrder(page, orderId) {
 	await page.goto(`/wp-admin/admin.php?page=wc_pre_orders`);
 	await page
 		.locator(
-			`#the-list th.check-column input[name="order_id[]"][value="${orderId}"]`
+			`#the-list .check-column input[name="order_id[]"][value="${orderId}"]`
 		)
 		.check();
 	await page.locator('#bulk-action-selector-top').selectOption('complete');
